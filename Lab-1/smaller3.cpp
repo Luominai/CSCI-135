28fdf581fe4f6dba557ce176673daac4a0020121
KevinLi0711 <108951684+Luominai@users.noreply.github.com>
2023-08-30 18:40:48 -0400

    fix

--- Lab-1/smaller3.cpp
+++ Lab-1/smaller3.cpp
@@ -24,13 +24,13 @@ int main(void) {
 
     int min = input1;
     if (input2 < min) {
-    min = input2;
+        min = input2;
     }
     if (input3 < min) {
-    min = input3;
+        min = input3;
     }
 
-    cout << "The smallest of the three is " << input3;
+    cout << "The smallest of the three is " << min;
 
     return 0;
 };
